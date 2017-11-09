bd6984e4932db8b24ef7e75b905352112ab2c5c2
Martin Indra <martin.indra@mgn.cz>
2017-11-09 14:00:30 +0100

    Emacs: setup ag to be called with --hidden
    Better not to miss matches in hidden files and directories. Some files
    are still ignored thanks to ``/agignore``.

--- emacs/init.el
+++ emacs/init.el
@@ -154,6 +154,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.geojson\\'" . json-mode))
 
+;; the silver search (ag.el)
+
+(add-to-list 'ag-arguments "--hidden")
+
 ;; so emacsclient can be used
 (require 'server)
 (unless (server-running-p)
