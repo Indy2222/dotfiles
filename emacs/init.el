5ffbb995668c9f9e13d396f463929d65b3f1ae61
Martin Indra <martin.indra@mgn.cz>
2019-03-12 20:45:59 +0100

    Emacs: Fix Rust competition
    Works only when company-mode is enabled, see
    https://github.com/racer-rust/emacs-racer

--- emacs/init.el
+++ emacs/init.el
@@ -262,6 +262,7 @@
     :config
     (add-hook 'rust-mode-hook #'racer-mode)
     (add-hook 'racer-mode-hook #'eldoc-mode)
+    (add-hook 'racer-mode-hook #'company-mode)
     (define-key rust-mode-map (kbd "C-c d") #'racer-describe)))
 
 ;; awesome Emacs interface to Git porcelain
