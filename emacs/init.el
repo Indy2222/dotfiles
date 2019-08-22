26d6dd5d7bed9d96e12743cacc7af12c575c7d0d
Martin Indra <martin.indra@mgn.cz>
2019-08-22 23:31:26 +0200

    Emacs: add powershell mode

--- emacs/init.el
+++ emacs/init.el
@@ -446,6 +446,10 @@
   :demand t
   :ensure t)
 
+(use-package powershell
+  :demand t
+  :ensure t)
+
 (defun indy/kill-file-name ()
   "Place buffer-file-name kill ring and display it."
   (interactive)
