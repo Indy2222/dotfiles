11d2bb2d034a72ed52544a83cc737abeee14a47b
Martin Indra <martin.indra@mgn.cz>
2021-04-05 13:24:02 +0200

    offlineimap: sync more directories

--- offlineimap.py
+++ offlineimap.py
@@ -1,9 +1,20 @@
 #! /usr/bin/env python2
 from subprocess import check_output
 
+GMAIL_EXCLUDE = {
+    '[Gmail]',
+    '[Gmail]/All Mail',
+    '[Gmail]/Important',
+    '[Gmail]/Spam',
+    '[Gmail]/Starred',
+    '[Gmail]/Bin',
+}
+
 GMAIL_NAME_MAPPING = {
     '[Gmail]/Sent Mail': 'Sent',
-    '[Gmail]/Drafts': 'Drafts'
+    '[Gmail]/Drafts': 'Drafts',
+    '[Gmail]/Trash': 'Trash',
+    '[Gmail]/Subscriptions': 'Subscriptions',
 }
 GMAIL_NAME_MAPPING_REVERSE = {v: k for k, v in GMAIL_NAME_MAPPING.iteritems()}
 
