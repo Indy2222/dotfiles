9f6d64813ffa05ba275f2f82d6926f07ad346ce6
Martin Indra <martin.indra@mgn.cz>
2022-04-11 21:33:07 +0200

    Mail: sync only some Datamole folders

--- offlineimap.py
+++ offlineimap.py
@@ -1,16 +1,8 @@
 #! /usr/bin/env python3
 from subprocess import check_output
 
-GMAIL_EXCLUDE = {
-    '[Gmail]',
-    '[Gmail]/All Mail',
-    '[Gmail]/Important',
-    '[Gmail]/Spam',
-    '[Gmail]/Starred',
-    '[Gmail]/Bin',
-}
-
 GMAIL_NAME_MAPPING = {
+    'INBOX': 'INBOX',
     '[Gmail]/Sent Mail': 'Sent',
     '[Gmail]/Drafts': 'Drafts',
     '[Gmail]/Trash': 'Trash',
