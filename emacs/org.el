914b711196275a409fac659c508dfb751c8362dd
Martin Indra <martin.indra@mgn.cz>
2022-04-21 22:47:18 +0200

    Emacs: bind org-clock-out & org-clock-in-last globally

--- emacs/org.el
+++ emacs/org.el
@@ -5,6 +5,8 @@
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c C-x C-o") 'org-clock-out)
+(global-set-key (kbd "C-c C-x C-x") 'org-clock-in-last)
 
 (setq
  org-catch-invisible-edits 'error
